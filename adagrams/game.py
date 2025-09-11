5bc8486d91ca423f2f826cb43d1298ed9ff1602d
Priscilla Andresen <pgdamorim@gmail.com>
2025-09-11 15:02:58 -0700

    Changes made in game.py first wave!

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,51 @@
 from random import randint
 
 def draw_letters():
-    pass
+
+    letter_available = {
+                    1:  {'A': 9},
+                    2:  {'B': 2},
+                    3:  {'C': 2},
+                    4:  {'D': 4},
+                    5:  {'E': 12},
+                    6:  {'F': 2},
+                    7:  {'G': 3},
+                    8:  {'H': 2},
+                    9:  {'I': 9},
+                    11: {'K': 1},
+                    12: {'L': 4},
+                    13: {'M': 2},
+                    14: {'N': 6},
+                    15: {'O': 8},
+                    16: {'P': 2},
+                    17: {'Q': 1},
+                    18: {'R': 6},
+                    19: {'S': 4},
+                    20: {'T': 6},
+                    21: {'U': 4},
+                    22: {'V': 2},
+                    23: {'W': 2},
+                    24: {'X': 1},
+                    25: {'Y': 2},
+                    26: {'Z': 1}
+    }
+
+    hand = []
+    #letter_available_copy = letter_available.copy()
+
+    while len(hand) < 10:
+        picked_letter = randint(1, len(letter_available))
+        print(picked_letter)
+        for num, inner_dict in letter_available.items():
+            if num == picked_letter:
+                for letter, num_available, in inner_dict.items():
+                    if num_available != 0:
+                        hand.append(letter)
+                        inner_dict[letter] = num_available - 1
+
+    return hand
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -11,3 +55,6 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
+draw_letters()
